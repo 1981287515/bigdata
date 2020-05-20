c964eeeb7017c4e574ea7a83bc2824e6c869ea4b
1981287515 <1981287515@qq.com>
2020-05-20 17:55:54 +0800

    第一次任务

---
+++ src/main/java/test/deal21.java
@@ -0,0 +1,39 @@
+package test;
+
+import com.csvreader.CsvReader;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class deal21 {
+    public static void main(String[] args) throws Exception {
+        FileWriter fw = null;
+        BufferedWriter bufw = null;
+        File file = new File("F:\\sparkdata\\大数据课程设计\\任务\\jobs2.csv");
+        File output = new File("F:\\sparkdata\\大数据课程设计\\任务\\xjobs2.txt");
+        fw = new FileWriter(output);
+        bufw = new BufferedWriter(fw);
+        InputStream in = new FileInputStream(file);
+        CsvReader cr = new CsvReader(in, Charset.forName("UTF-8"));
+        cr.readHeaders();
+        bufw.write(Arrays.toString(cr.getHeaders())+"\n");
+        //cr.readRecord();
+        while(cr.readRecord()){
+            int columnCount = cr.getColumnCount();
+            for (int i = 0; i < columnCount; i++) {
+                String str = cr.get(i);
+                Pattern p = Pattern.compile("\\s+|\t+|\n|\r");
+                Matcher m = p.matcher(str);
+                String s = m.replaceAll("");
+                bufw.write(s+"/~/");
+                System.out.println(s);
+            }
+            bufw.write("\n");
+            bufw.flush();
+        }
+         cr.close();
+    }
+}
---
+++ src/main/java/test/deal42.java
@@ -0,0 +1,122 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+
+class deal42 {
+
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        InputStreamReader isr = null;
+        BufferedReader bufr = null;
+
+        String inputPath = "F:\\sparkdata\\大数据课程设计\\任务\\xjobs4.txt";
+
+        FileWriter fw = null;
+        BufferedWriter bufw = null;
+        String outPath = "F:\\sparkdata\\大数据课程设计\\任务\\xxjobs4(1).csv";
+        int dataCount = 0;
+        try {
+
+            fw = new FileWriter(outPath);
+            bufw = new BufferedWriter(fw);
+
+            fis = new FileInputStream(inputPath);
+
+
+            isr = new InputStreamReader(fis);
+
+            bufr = new BufferedReader(isr);
+
+            String line = bufr.readLine();
+            String str = "";
+            while(line != null) {
+
+                if(line.startsWith("[")) {
+                    str = "";
+                    line = bufr.readLine();
+                    continue ;
+                }
+
+                if(line.startsWith("company_financing_stage")) {
+                    str = "";
+                    line = bufr.readLine();
+                    continue ;
+                }
+
+                String[] strs = line.split("~~");
+                String word = "";
+                for(int i=0; i<strs.length; i++) {
+
+                    if(i == 0) {
+                        word += strs[i];
+                    }else {
+                        word += "///"+strs[i];
+                    }
+                }
+
+                String[] newStrs = word.split("///");
+                for(int i=0; i<newStrs.length; i++) {
+                    if(i==1 || i==3 || i==7 || i==11) {
+                        str += newStrs[i]+"///";
+                    }
+                }
+
+                str = str.substring(0, str.length()-3);
+                str=str.replace(",","，");
+                str=str.replace("///",",");
+                bufw.write(str);
+                bufw.newLine();
+                dataCount++;
+                str = "";
+                line = bufr.readLine();
+            }
+            if(bufr!=null){
+                try {
+                    bufr.close();
+                } catch (IOException e) {
+                }
+            }
+            if(isr!=null){
+                try {
+                    isr.close();
+                } catch (IOException e) {
+                }
+            }
+            if(fis!=null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                }
+            }
+
+            bufw.flush();
+            System.out.println("共处理:"+dataCount+"条数据");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(bufw!=null){
+                try {
+                    bufw.close();
+                } catch (IOException e) {
+                }
+            }
+            if(fw!=null){
+                try {
+                    fw.close();
+                } catch (IOException e) {
+                }
+            }
+
+        }
+    }
+
+}
+
